0c2c56f7dc17b6a7e983d81d2033f8913e7c9bc2
terminaldweller <thabogre@gmail.com>
2022-07-04 16:44:16 +0430

    update

---
+++ fib.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S deno run
+"use strict";
+// https://www.youtube.com/watch?v=oBt53YbR9Kk
+
+// memoization
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) {
+    return 1;
+  }
+  memo[n] = fib(n - 2, memo) + fib(n - 1, memo);
+  return memo[n];
+};
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(100));
---
+++ gridTraveller.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S deno run
+"use strict";
+
+const gridTraveller = (m, n, memo = {}) => {
+  const key = m + "," + n;
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  memo[key] = gridTraveller(m - 1, n, memo) + gridTraveller(m, n - 1, memo);
+  return memo[key];
+};
+
+console.log(gridTraveller(2, 3));
+console.log(gridTraveller(2, 2));
+console.log(gridTraveller(3, 3));
+console.log(gridTraveller(10, 10));
+console.log(gridTraveller(20, 10));
