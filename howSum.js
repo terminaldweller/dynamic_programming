aee9427f3ea5418cbbc368e39ed985918bf34263
terminaldweller <thabogre@gmail.com>
2022-08-02 10:06:29 +0430

    update

---
+++ howSum.js
@@ -0,0 +1,27 @@
+#!/home/devi/.bun/bin/bun
+
+var howSum = function (targetSum, numbers, memo = {}) {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const remainderResult = howSum(remainder, numbers, memo);
+    if (remainderResult !== null) {
+      memo[targetSum] = [...remainderResult, num];
+      return memo[targetSum];
+    }
+  }
+
+  memo[targetSum] = null;
+  return null;
+};
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(300, [7, 14]));
+console.log(howSum(301, [7, 14]));
+console.log(howSum(308, [7, 14]));
