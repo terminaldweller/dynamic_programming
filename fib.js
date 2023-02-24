0b9a607b8ce63fb27b05ff591801daa875b8d830
terminaldweller <thabogre@gmail.com>
2023-02-24 20:09:09 +0330

    foad

--- fib.js
+++ fib.js
@@ -5,7 +5,7 @@
 // memoization
 const fib = (n, memo = {}) => {
   if (n in memo) return memo[n];
-  if (n <= 2) {
+  if (n <= 1) {
     return 1;
   }
   memo[n] = fib(n - 2, memo) + fib(n - 1, memo);
